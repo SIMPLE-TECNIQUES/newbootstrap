1f5870187efedf7ff74124434fc3257d2c46b827
SIMPLE-TECNIQUES <shankarcode2000@gmail.com>
2024-05-18 17:00:16 +0530

    first commit

---
+++ src/Component/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Card = ({ element }) => {
+  return (
+    <div className="col-sm-6 col-md-4 col-lg-4 col-xl-4 mb-3">
+      <div className="card">
+        <img className="card-img-top img-fluid" src={element.image} alt="Card image" />
+        <div className="card-body">
+          <h2 className="card-text text-center">{element.head}</h2>
+          <h3 className="card-text content text-center">{element.content}</h3>
+          <h5 className="card-text text-center">{element.date}</h5>
+          <h5 className="card-text text-center">{element.availableIn}</h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/Pages/All.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from '../Component/Card';
+
+const All = ({ data }) => {
+  return (
+    <div className="container">
+      <h1 className="text-center my-4">All Courses</h1>
+      <div className="row">
+        {data.map((element, index) => (
+          <Card key={element.id} element={element} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default All;
